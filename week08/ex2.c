e6b3685df59fbbff521add890521d1841abecf48
annastrcv <54358752+annastrcv@users.noreply.github.com>
2019-10-10 23:10:29 +0300

    Add files via upload

---
+++ week08/ex2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main ()
+{
+   for(int i = 0; i < 10; ++i){
+      void* temp = malloc(10*1024*1024);
+      memset(temp, 0, 10*1024*1024);
+      sleep(1);
+   }
+   return 0;
+}
